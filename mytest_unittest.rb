028cd36988504714430074373e91330eb431a998
David Contreras <davidcosama@users.noreply.github.com>
2018-08-17 13:28:38 +0200

    Update mytest_unittest.rb file
    To avoid the result list from the input box

--- mytest_unittest.rb
+++ mytest_unittest.rb
@@ -2,43 +2,32 @@ require 'test/unit'
 require 'selenium-webdriver'
 
 class MyTest < Test::Unit::TestCase
-
- def setup
+  def setup
   
     options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
-
-    @driver = Selenium::WebDriver.for(:chrome, options: options)
-
-    
-   
+    @driver = Selenium::WebDriver.for(:chrome, options: options) 
     @driver.manage.window.maximize
-
-    @driver.navigate.to "http://www.google.lk"
- end
-  
-  def test_search
-		  
-		  #Typing the Selenium in Google search
-		@driver.find_element(:id, "lst-ib").send_keys "Selenium"
-		
-		#Clicking the search button
-		@driver.find_element(:name, "btnK").click
-		
-
-		#Clicking the search button
-		@driver.find_element(:link, "Selenium - Web Browser Automation").click
-		
-
-		#Asserting whether the registration success message is diaplyed
-	    puts @driver.title
-		
-
+    @driver.navigate.to "https://www.google.lk"
   end
   
+  def test_search
+    #Typing the Selenium in Google search
+    @driver.find_element(:id, "lst-ib").send_keys "Selenium"
+
+    #Press Key Esc to avoid the list of results from the input box
+    @driver.find_element(:id, "lst-ib").send_keys "\ue00C"
+
+    #Clicking the search button
+    @driver.find_element(:name, "btnK").click
+
+    #Clicking the search button
+    @driver.find_element(:link, "Selenium - Web Browser Automation").click
+
+    #Asserting whether the registration success message is displayed
+    puts @driver.title
+  end
+
   def teardown
 		@driver.quit
   end
- 
-
- 
 end
